f1683b3bd1da13df7779bbe9e51609beea8eb36d
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-05 03:07:23 +0300

    Add add paper sheet edges detection and image transformation to fit paper sheet coordinates

--- main.cpp
+++ main.cpp
@@ -61,9 +61,81 @@ void drawCoinInfo(cv::Mat& image, const cv::Point& center, int radius, int value
     drawCoinLocation(image, center, radius);
 }
 
-std::vector<cv::Point2i> getPaperSheetCoordinates(const cv::Mat& image) {
-    
+cv::Point2i findClosestTo(const cv::Point2i& point, const std::vector<cv::Point2i>& points) {
+    double minDist = 1000000;
+    cv::Point2i closestPoint;
+    for (const auto &p : points) {
+        double distance = pow(pow(point.x - p.x, 2) + pow(point.y - p.y, 2), 0.5);
+        if(distance < minDist) {
+            closestPoint = p;
+            minDist = distance;
+        }
+    }
+    return closestPoint;
+}
 
+
+std::vector<cv::Point2i> getPaperSheetCoordinates(const cv::Mat& image) {
+    cv::Mat hsvImg, thresholdedImg;
+    cv::cvtColor(image, hsvImg, cv::COLOR_BGR2HSV);
+
+    cv::inRange(hsvImg, cv::Scalar(0, 0, 100), cv::Scalar(255, 75, 255), thresholdedImg);
+    //cv::Canny(thresholdedImg, thresholdedImg, 50, 200, 3);
+
+
+    cv::Mat t;
+    cv::bitwise_and(image, image, t, thresholdedImg);
+
+    cv::Mat blured;
+    cv::GaussianBlur(t, blured, cv::Size(5, 5), 3);
+
+
+
+    cv::namedWindow("sdad");
+    cv::imshow("sdad", blured);
+    cv::waitKey(0);
+
+
+    std::vector<cv::Point2i> corners;
+    int maxCorners = 100;
+    double qualityLevel = 0.01;
+    double minDistance = 10;
+    int blockSize = 3;
+    bool useHarrisDetector = true;
+    double k = 0.04;
+
+    cv::Mat grayImg;
+    cv::cvtColor(blured, grayImg, cv::COLOR_BGR2GRAY);
+    /// Apply corner detection
+    cv::goodFeaturesToTrack( grayImg,
+                         corners,
+                         maxCorners,
+                         qualityLevel,
+                         minDistance,
+                         thresholdedImg,
+                         blockSize,
+                         useHarrisDetector,
+                         k );
+
+    for (const auto &corner : corners) {
+        cv::circle(image, corner, 10, cv::Scalar(255, 0, 0), 3);
+    }
+
+
+
+    std::vector<cv::Point2i> extremes;
+
+    extremes.push_back(findClosestTo(cv::Point(0, 0), corners));
+    extremes.push_back(findClosestTo(cv::Point(0, image.rows - 1), corners));
+    extremes.push_back(findClosestTo(cv::Point(image.cols - 1, image.rows - 1), corners));
+    extremes.push_back(findClosestTo(cv::Point(image.cols, 0), corners));
+
+    //if(cv::norm(extremes[2] - extremes[1]) < cv::norm(extremes[2] - extremes[3])) {
+        //extremes.erase(extremes.begin() + 2);
+    //} else {
+        extremes.erase(extremes.begin() + 3);
+    //}
+    return extremes;
 }
 
 cv::Mat getPaperSheetTransformationMatrix(const cv::Mat& image) {
@@ -73,13 +145,13 @@ cv::Mat getPaperSheetTransformationMatrix(const cv::Mat& image) {
     cv::Point2f srcPoint[3];
     cv::Point2f dstPoint[3];
 
-    srcPoint[1] = coordinates[0];
+    srcPoint[0] = coordinates[0];
     srcPoint[1] = coordinates[1];
-    srcPoint[1] = coordinates[2];
+    srcPoint[2] = coordinates[2];
 
     dstPoint[0] = cv::Point2f(0, 0);
-    dstPoint[1] = cv::Point2f(image.cols - 1, 0 );
-    dstPoint[2] = cv::Point2f(0, image.rows - 1 );
+    dstPoint[1] = cv::Point2f(0, image.rows - 1);
+    dstPoint[2] = cv::Point2f(image.cols - 1, image.rows - 1);
 
     cv::Mat transformMatrix = cv::getAffineTransform(srcPoint, dstPoint);
 
@@ -98,7 +170,9 @@ cv::Mat getPaperSheetRegion(const cv::Mat& image) {
 }
 
 int main() {
-    cv::Mat image = cv::imread("coins2.jpg", 1);
+    cv::Mat image = cv::imread("coins5.jpg", 1);
+
+    cv::Mat region = getPaperSheetRegion(image);
 
     std::vector<cv::Vec3f> circles = getCoinLocations(image);
 
@@ -113,7 +187,7 @@ int main() {
         drawCoinInfo(image, center, radius, coinValue);
     }
 
-    imshow("Coin Recognition", image );
+    imshow("Coin Recognition", region );
 
     cv::waitKey(0);
     return 0;
